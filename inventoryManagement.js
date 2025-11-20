11200a2896a72a92c7c6a9db352147c71e33120b
abdimkeyhassan-ctrl <abdimkeyhassan@gmail.com>
2025-11-20 15:25:56 +0300

    Added functions to manage products array

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,8 +1,43 @@
 // Write your code here
+const products = [
+  "Laptop",
+  "phone",
+  "headphones",
+  "Monitor",
+];
+function logFirstProduct() {
+  console.log(products[0]);
+  console.log(products);
+}
+
+
+function addProduct(Laptop) {
+  products.push(Laptop);
+}
+function updateProductName(index, newName) {
+  if (index >= 0 && index < products.length) {
+    products[index] = newName;
+  } else {
+    console.log("Invalid product index");
+  }
+}
+function removeLastProduct() {
+  products.pop();
+}
 
 
 
-// Export the necessary parts for testing
+
+
+
+
+
+
+
+
+// Export the necessary parts for testingfunction removeLastProduct() {
+  
+
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
   addProduct: typeof addProduct !== 'undefined' ? addProduct : undefined,
